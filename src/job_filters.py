4e1b618390f0dccf150e7879d96617bcc94bcdad
Himanshu Singh <118012202+himanshudev28@users.noreply.github.com>
2026-06-05 13:12:43 +0530

    Add job filters with keywords and locations
    Define constants for job keywords, locations, and max experience.

---
+++ src/job_filters.py
@@ -0,0 +1,23 @@
+KEYWORDS = [
+    "frontend developer",
+    "react developer",
+    "next.js developer",
+    "software engineer",
+    "associate software engineer",
+    "graduate engineer trainee",
+    "full stack developer",
+    "mern developer",
+    "java developer"
+]
+
+LOCATIONS = [
+    "remote",
+    "noida",
+    "gurugram",
+    "delhi",
+    "bangalore",
+    "pune",
+    "hyderabad"
+]
+
+MAX_EXPERIENCE = 2
