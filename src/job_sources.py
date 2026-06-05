d2174d26eb8d759adc817e40f521f6d30ee8909f
Himanshu Singh <118012202+himanshudev28@users.noreply.github.com>
2026-06-05 13:24:33 +0530

    Create job_sources.py

---
+++ src/job_sources.py
@@ -0,0 +1,19 @@
+KEYWORDS = [
+    "frontend developer",
+    "react developer",
+    "next.js developer",
+    "software engineer",
+    "associate software engineer",
+    "full stack developer",
+    "java developer",
+]
+
+LOCATIONS = [
+    "remote",
+    "india",
+    "noida",
+    "gurugram",
+    "bangalore",
+    "hyderabad",
+    "pune",
+]
